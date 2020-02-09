ba8edb965b107ab096ebca4495945591adf3d50f
Cherry Ng <cherry92@gmail.com>
2020-02-09 23:57:47 +0800

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "${DIR}"
+
+symlink() {
+  local src="$1"
+  local dest="$2"
+  echo "Symlink ${dest} -> ${src}"
+  mkdir -p "$(dirname ${dest})"
+  rm -rf "${dest}"
+  ln -s "${src}" "${dest}"
+}
+
+git clone https://github.com/rime/plum.git
+cd "${DIR}/plum"
+"${DIR}/plum/rime-install" "cherrry/rime-cangjie" "jyutping"
+
+RIME_DIR="${HOME}/.config/ibus/rime"
+if [ `uname` == "Darwin" ]; then
+  RIME_DIR="${HOME}/Library/Rime"
+fi
+mkdir -p "${RIME_DIR}"
+
+CONFIG_DIR="${DIR}/config"
+symlink "${CONFIG_DIR}/cangjie5.cj3ext.dict.yaml" "${RIME_DIR}/cangjie5.cj3ext.dict.yaml"
+symlink "${CONFIG_DIR}/cangjie5.custom.yaml" "${RIME_DIR}/cangjie5.custom.yaml"
+symlink "${CONFIG_DIR}/default.custom.yaml" "${RIME_DIR}/default.custom.yaml"
+symlink "${CONFIG_DIR}/jyutping.custom.yaml" "${RIME_DIR}/jyutping.custom.yaml"
+symlink "${CONFIG_DIR}/squirrel.custom.yaml" "${RIME_DIR}/squirrel.custom.yaml"
+
+echo "Please manually deploy rime again."
