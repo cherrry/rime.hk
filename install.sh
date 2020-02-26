b0f554f517b42758f0c5ac4f1efbc0665a310b89
Cherry Ng <cherry92@gmail.com>
2020-02-26 19:44:36 +0800

    Migrate to rime-cantonese.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ symlink() {
 
 git clone https://github.com/rime/plum.git
 cd "${DIR}/plum"
-"${DIR}/plum/rime-install" "cherrry/rime-cangjie" "jyutping"
+"${DIR}/plum/rime-install" "cherrry/rime-cangjie" "cantonese"
 
 RIME_DIR="${HOME}/.config/ibus/rime"
 if [ `uname` == "Darwin" ]; then
@@ -26,7 +26,7 @@ CONFIG_DIR="${DIR}/config"
 symlink "${CONFIG_DIR}/cangjie5.cj3ext.dict.yaml" "${RIME_DIR}/cangjie5.cj3ext.dict.yaml"
 symlink "${CONFIG_DIR}/cangjie5.custom.yaml" "${RIME_DIR}/cangjie5.custom.yaml"
 symlink "${CONFIG_DIR}/default.custom.yaml" "${RIME_DIR}/default.custom.yaml"
-symlink "${CONFIG_DIR}/jyutping.custom.yaml" "${RIME_DIR}/jyutping.custom.yaml"
+symlink "${CONFIG_DIR}/jyut6ping3.custom.yaml" "${RIME_DIR}/jyut6ping3.custom.yaml"
 symlink "${CONFIG_DIR}/squirrel.custom.yaml" "${RIME_DIR}/squirrel.custom.yaml"
 
 echo "Please manually deploy rime again."
